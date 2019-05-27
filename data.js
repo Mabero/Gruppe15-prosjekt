b1f61f8f4991613491f181b10eb378d9f7436167
Stian Greiner Haaker <42611069+Stianhaaker@users.noreply.github.com>
2019-05-27 12:33:39 +0200

    datastruktur test yo

---
+++ data.js
@@ -0,0 +1,46 @@
+let cards = [
+
+    {
+        id: document.getElementById("input tekstfelt i html kode").innerHTML = team.push[0];,
+        name:"et eller annet",
+        status: to do, done, osv,
+        description: "":
+    },
+
+    {
+    },
+
+    {
+    },
+
+    {
+    },
+
+    {
+    },
+
+    {
+    },
+
+    {
+    },
+
+];
+
+
+
+
+let team = [
+    { id: 1, name:},
+    { id: 2, name:},
+    { id: 3, name:},
+    { id: 4, name:},
+    { id: 5, name:},
+];
+
+
+
+
+let assignments = [
+  
+];
