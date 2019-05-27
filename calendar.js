761565387d2881d6c2a7f9d380fe389c7b162ad5
enkefar <even.solheim@hotmail.com>
2019-05-27 14:44:53 +0200

    Forbedret prosjektfanen + litt formatering og kommentering

--- calendar.js
+++ calendar.js
@@ -3,3 +3,14 @@ document.getElementById("calendarButton").addEventListener("click", testing);
 function testing() {
   alert("Kalender")
 }
+
+
+let textnode = document.createTextNode(myFunction);
+
+function myFunction() {
+  document.getElementById("frm1").submit();
+  var el = document.querySelector('frm1');
+  var newEl = document.createElement('p');
+  newEl.innerHTML = document.getElementById("frm1")();
+  el.parentNode.replaceChild(newEl, el);
+}
