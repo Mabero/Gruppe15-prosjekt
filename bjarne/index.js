911df993894abe3223c40fad8136bb4b4d289461
bjarnehaugen <bjarnewilhelmhaugen@hotmail.com>
2019-05-28 13:08:27 +0200

    updated drop and drag code

--- bjarne/index.js
+++ bjarne/index.js
@@ -8,7 +8,23 @@ function drag(ev) {
 
 function drop(ev) {
     ev.preventDefault();
-    if ( event.target.className == "list" ) {
+    if ( ev.target.className == "list")  {
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+}
+
+function allowDrop1(ev) {
+    ev.preventDefault();
+}
+
+function drag1(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop1(ev) {
+    ev.preventDefault();
+    if ( event.target.className == "listWrap" ) {
         var data = ev.dataTransfer.getData("text");
         ev.target.appendChild(document.getElementById(data));
     }
