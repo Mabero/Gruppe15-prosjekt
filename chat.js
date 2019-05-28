fad0442fd148f5dc03c91cf1e539aae79c467518
fazalata <fazal.123@hotmail.com>
2019-05-28 17:12:28 +0200

    updated js file.

--- chat.js
+++ chat.js
@@ -5,3 +5,17 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+var inputElement = document.getElementById("input");
+var msgElement = document.getElementById("msg");
+
+
+var btnElement = document.getElementById("btn-1");
+
+btnElement.addEventListener("click", function() {
+	if(inputElement.value === "hei"){
+		msg.innerText ="hdhdhhd";
+	}
+       });
+
+
