94a31b0045d83b38c6ba27fceb2ac1276aafc101
bjarnehaugen <bjarnewilhelmhaugen@hotmail.com>
2019-05-28 09:46:57 +0200

    fixed drop and drag code, drag now working

--- bjarne/index.js
+++ bjarne/index.js
@@ -1,16 +1,12 @@
-var test1 = document.getElementsByClassName("list");
-
-if test1 {
-    function allowDrop(ev) {
-        ev.preventDefault();
-    }
+function allowDrop(ev) {
+    ev.preventDefault();
 }
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev)
+function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
