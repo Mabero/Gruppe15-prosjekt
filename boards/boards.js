2ab9532e2f9ef850ff87ac6471b595e4edc12003
Mats B Rossing <mats.brossing@gmail.com>
2019-05-30 14:54:21 +0200

    Finished board page for now.

--- boards/boards.js
+++ boards/boards.js
@@ -1,16 +1,45 @@
 const boardsBtn = document.getElementById("boardsBtn");
 const boardsContainer = document.getElementById("boardsContainer");
 
+let boards = [];
+
 boardsBtn.addEventListener("click", function(){
-    let newDiv = document.createElement("div")
-    let newContent = document.createTextNode("test")
+    const newA = document.createElement("a");
+    const removeA = document.createElement("a");
+    const newDiv = document.createElement("div");
+    const newInputField = document.createElement("input");
+
+    newInputField.placeholder = "Enter board name ..."
     newDiv.className = "board";
-    cards.push(newDiv)
-    newDiv.appendChild(newContent);
+    newDiv.appendChild(newInputField);
     boardsContainer.appendChild(newDiv);
+
+    newA.textContent = "Go to board"
+    removeA.textContent = "Remove board";
+
+    newDiv.appendChild(removeA);
+    newDiv.appendChild(newA);
+
+    newA.href = "https://www.vg.no"
+    newA.className = "boardA";
+    removeA.className = "removeA";
+    removeA.href = "javascript:removeBoard()";
+
+    newA.after(removeA);
     newDiv.after(boardsBtn);
+
+    boardsContainer.appendChild(boardsBtn);
+    boards.push(newDiv)
+    for(i = 0; i < boards.length; i++) {
+        boards[i].setAttribute("id", "board" + i);
+        boards[i].childNodes[2].setAttribute("onclick", "removeBoard("+ i +")")
+    }
 })
 
-let cards = [];
+function removeBoard(i) {
+    const board = document.getElementById("board" + i)
+    board.remove();
+}
 
-let list = document.get
+
+
