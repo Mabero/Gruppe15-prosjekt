adea8ebe8c285c24e407f65eb35e15c187691b43
enkefar <even.solheim@hotmail.com>
2019-06-03 16:40:51 +0200

    Ferdig-ish stylized. Fikset en bug som gjorde at pop-up'en alltid var up.

--- calendar.js
+++ calendar.js
@@ -1,19 +1,23 @@
+// Funksjon som viser hvilken dato det er i dag, og displayer den først i flexen.
+
 var d = new Date();
 
 
-function daysInMonth (month, year) {
-  return new Date(year, month, 0).getDate();
+function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
 }
 
 
-for(i = d.getDate(); i < daysInMonth(d.getMonth(), d.getFullYear()); i++){
-  console.log(i)
-  const node = document.createElement("div");
-  node.innerHTML = i;
-  document.getElementById("daysContainer").appendChild(node);
+for (i = d.getDate(); i < daysInMonth(d.getMonth(), d.getFullYear()); i++) {
+    const node = document.createElement("div");
+    node.innerHTML = i;
+    document.getElementById("daysContainer").appendChild(node);
 }
 
 
+
+
+// Månedsnavn for å ha over kalenderen. Sier ifra hvilken måned du er i.
 var m = new Date();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("month").innerHTML = months[m.getMonth()];
@@ -22,15 +26,15 @@ document.getElementById("month").innerHTML = months[m.getMonth()];
 
 
 
-    
+
 // Åpner en task med en pop-up
 
 function openForm() {
-  document.getElementById("project1").style.display = "block";
-  document.getElementById("project1").style.transition = "2s all ease-out";
+    document.getElementById("project1").style.display = "block";
+    document.getElementById("project1").style.transition = "2s all ease-out";
 }
 
 function closeForm() {
-  document.getElementById("project1").style.display = "none";
-  document.getElementById("project1").style.transition = "2s all ease-out";
+    document.getElementById("project1").style.display = "none";
+    document.getElementById("project1").style.transition = "2s all ease-out";
 }
