9c4a748178b240eaf68f823f3b13c0b806782a7c
Mabero <42588003+Mabero@users.noreply.github.com>
2019-06-06 11:08:19 +0200

    Delete description.js

--- description.js
+++
@@ -1,9 +0,0 @@
-
-
-
-function text2 (){
-    var inputText = document.getElementsByName("tekst")[0].value;
-    var newText = document.createElement("p");
-    document.body.appendChild(newText);
-    newText.textContent = inputText;
-}
