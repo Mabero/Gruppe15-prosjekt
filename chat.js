859e8b8f627baf0d398d135d4239105323e2a81f
fazalata <fazal.123@hotmail.com>
2019-06-06 15:17:58 +0200

    edit close button

--- chat.js
+++ chat.js
@@ -3,12 +3,15 @@ function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
-
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 
 // Hente verdier
 var inputElement = document.getElementById("input");
 var outputElement = document.getElementById("output");
 var btnElement = document.getElementById("btn-1");
+
 
 // Få brukern til å sende meldingen sin og kalle brukern for user
 btnElement.addEventListener("click", function(event){
